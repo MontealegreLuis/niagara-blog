da2e1889dbefc692ae26619fd8388a5f98e91621
Luis Montealegre <montealegreluis@gmail.com>
2018-04-02 10:54:04 -0500

    refactor: Cache the post owner expression

--- src/main/java/com/codeup/blog/PostOwnerExpression.java
+++ src/main/java/com/codeup/blog/PostOwnerExpression.java
@@ -3,18 +3,18 @@
  */
 package com.codeup.blog;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostOwnerExpression {
     private Posts posts;
 
-    @Autowired
     public PostOwnerExpression(Posts posts) {
         this.posts = posts;
     }
 
+    @Cacheable(value = "is-post-author", key = "#currentUser.id + '_' + #postId")
     public boolean isAuthor(User currentUser, Long postId) {
         Post post = posts.findOne(postId);
         return post.isAuthoredBy(currentUser);
